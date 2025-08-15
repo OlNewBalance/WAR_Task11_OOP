6f32c67af97dd8711525415a17674cbd5880f2a6
Oleg <finkton23@gmail.com>
2025-08-15 20:41:37 +0500

    Update Cavalry.cs

--- WAR_Task13_OOP/Cavalry.cs
+++ WAR_Task13_OOP/Cavalry.cs
@@ -1,6 +1,12 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
 public class Cavalry : Soldier
 {
-        
+    public Cavalry()
+        : base(healthPoint: 100, defense: 30, maxAttack: 40, minAttack: 25, type: "Cavalry") { }
+
+    public Cavalry(int hp, int def, int maxAtk, int minAtk)
+        : base(hp, def, maxAtk, minAtk, "Cavalry") { }
+
+    public override Soldier Clone() => new Cavalry(HealthPoint, Defense, MaxAttack, MinAttack);
 }

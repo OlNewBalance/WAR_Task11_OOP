7075000777403a2d87e8d012265dcf6599b49795
Oleg <finkton23@gmail.com>
2025-08-15 20:42:38 +0500

    Update Infantry.cs

--- WAR_Task13_OOP/Infantry.cs
+++ WAR_Task13_OOP/Infantry.cs
@@ -1,6 +1,12 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
 public class Infantry : Soldier
 {
-        
+    public Infantry()
+        : base(healthPoint: 100, defense: 20, maxAttack: 25, minAttack: 15, type: "Infantry") { }
+
+    public Infantry(int hp, int def, int maxAtk, int minAtk)
+        : base(hp, def, maxAtk, minAtk, "Infantry") { }
+
+    public override Soldier Clone() => new Infantry(HealthPoint, Defense, MaxAttack, MinAttack);
 }

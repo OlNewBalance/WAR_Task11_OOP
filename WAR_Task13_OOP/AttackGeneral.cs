1662cf357e6c525b1f58f26d8ab30c57c36e3fe3
Oleg <finkton23@gmail.com>
2025-08-15 20:41:19 +0500

    Update AttackGeneral.cs

--- WAR_Task13_OOP/AttackGeneral.cs
+++ WAR_Task13_OOP/AttackGeneral.cs
@@ -1,13 +1,42 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
-public class General
+public class AttackGeneral
 {
-        public string GeneralName { get;}
-        public int GeneralAttackBuff;
-        public int GeneralDefenseBuff;
+    public static string GeneralName { get; set; }
+    public static int GeneralAttackBuff;
 
-        public General(string GeneralName, int generalAttackBuff, int generalDefenseBuff)
+    public AttackGeneral(string generalName, int generalAttackBuff)
+    {
+        GeneralName = generalName;
+        generalName = Data.PlayerGeneralNameInput;
+        GeneralAttackBuff = generalAttackBuff;
+        generalAttackBuff = 5;
+    }
+
+    public static void AttackBuff()
+    {
+        if (Data.GeneralIsAttack == true)
         {
-                GeneralName = "";
+            Data.myArtillery.MaxAttack += GeneralAttackBuff;
+            Data.myArtillery.MinAttack += GeneralAttackBuff;
+            Data.myInfantry.MaxAttack += GeneralAttackBuff;
+            Data.myInfantry.MinAttack += GeneralAttackBuff;
+            Data.myCavalry.MaxAttack += GeneralAttackBuff;
+            Data.myCavalry.MinAttack += GeneralAttackBuff;
+            Data.myGuard.MaxAttack += GeneralAttackBuff;
+            Data.myGuard.MinAttack += GeneralAttackBuff;
         }
+
+        if (Data.GeneralIsSelectedByEnemy == true)
+        {
+            Data.enemyArtillery.MaxAttack += GeneralAttackBuff;
+            Data.enemyArtillery.MinAttack += GeneralAttackBuff;
+            Data.enemyInfantry.MaxAttack += GeneralAttackBuff;
+            Data.enemyInfantry.MinAttack += GeneralAttackBuff;
+            Data.enemyCavalry.MaxAttack += GeneralAttackBuff;
+            Data.enemyCavalry.MinAttack += GeneralAttackBuff;
+            Data.enemyGuard.MaxAttack += GeneralAttackBuff;
+            Data.enemyGuard.MinAttack += GeneralAttackBuff;
+        }
+    }
 }

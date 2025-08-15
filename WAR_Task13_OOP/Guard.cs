95928cfb1e00cfd2d3bbe6f42245831e28f4382b
Oleg <finkton23@gmail.com>
2025-08-15 20:42:21 +0500

    Update Guard.cs

--- WAR_Task13_OOP/Guard.cs
+++ WAR_Task13_OOP/Guard.cs
@@ -1,6 +1,13 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
+
 
 public class Guard : Soldier
 {
-        
+    public Guard()
+        : base(healthPoint: 100, defense: 50, maxAttack: 40, minAttack: 30, type: "Guard") { }
+
+    public Guard(int hp, int def, int maxAtk, int minAtk)
+        : base(hp, def, maxAtk, minAtk, "Guard") { }
+
+    public override Soldier Clone() => new Guard(HealthPoint, Defense, MaxAttack, MinAttack);
 }

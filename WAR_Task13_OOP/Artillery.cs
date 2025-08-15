874b020d9b54302232fa6710653a23c595ba65b4
Oleg <finkton23@gmail.com>
2025-08-15 20:40:54 +0500

    Update Artillery.cs

--- WAR_Task13_OOP/Artillery.cs
+++ WAR_Task13_OOP/Artillery.cs
@@ -1,6 +1,12 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
 public class Artillery : Soldier
 {
-        
+    public Artillery()
+        : base(healthPoint: 100, defense: 0, maxAttack: 80, minAttack: 65, type: "Artillery") { }
+
+    public Artillery(int hp, int def, int maxAtk, int minAtk)
+        : base(hp, def, maxAtk, minAtk, "Artillery") { }
+
+    public override Soldier Clone() => new Artillery(HealthPoint, Defense, MaxAttack, MinAttack);
 }

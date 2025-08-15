0634017aa88f5b426d15a0b6d9425d86edb0fb31
Oleg <finkton23@gmail.com>
2025-08-15 20:42:03 +0500

    Update DeffGeneral.cs

--- WAR_Task13_OOP/DeffGeneral.cs
+++ WAR_Task13_OOP/DeffGeneral.cs
@@ -1,6 +1,40 @@
-﻿namespace WAR_Task13_OOP;
-
 public class DeffGeneral
 {
-    
+    public static string GeneralName { get; set; }
+    public static int GeneralDefenseBuff;
+
+    public DeffGeneral(string generalName, int generalDefenseBuff)
+    {
+        GeneralName = generalName;
+        generalName = Data.PlayerGeneralNameInput;
+        GeneralDefenseBuff = generalDefenseBuff;
+        generalDefenseBuff = 5;
+    }
+
+    public static void DeffenseBuff()
+    {
+        if (Data.GeneralIsDeff == true)
+        {
+            Data.myArtillery.Defense += GeneralDefenseBuff;
+            Data.myArtillery.Defense += GeneralDefenseBuff;
+            Data.myInfantry.Defense += GeneralDefenseBuff;
+            Data.myInfantry.Defense += GeneralDefenseBuff;
+            Data.myCavalry.Defense += GeneralDefenseBuff;
+            Data.myCavalry.Defense += GeneralDefenseBuff;
+            Data.myGuard.Defense += GeneralDefenseBuff;
+            Data.myGuard.Defense += GeneralDefenseBuff;
+        }
+
+        if (Data.GeneralIsSelectedByEnemy == true)
+        {
+            Data.enemyArtillery.Defense += GeneralDefenseBuff;
+            Data.enemyArtillery.Defense += GeneralDefenseBuff;
+            Data.enemyInfantry.Defense += GeneralDefenseBuff;
+            Data.enemyInfantry.Defense += GeneralDefenseBuff;
+            Data.enemyCavalry.Defense += GeneralDefenseBuff;
+            Data.enemyCavalry.Defense += GeneralDefenseBuff;
+            Data.enemyGuard.Defense += GeneralDefenseBuff;
+            Data.enemyGuard.Defense += GeneralDefenseBuff;
+        }
+    }
 }

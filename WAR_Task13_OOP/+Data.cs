cb7329806864f40a9e3a34df2055bd0228b67e74
Oleg <finkton23@gmail.com>
2025-08-15 20:38:34 +0500

    Update +Data.cs

--- WAR_Task13_OOP/+Data.cs
+++ WAR_Task13_OOP/+Data.cs
@@ -1,18 +1,42 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
 public static class Data
 {
-    public static bool GeneralIsSelected = false;
-    public static bool GeneralIsAttack = false;
-    public static bool GeneralIsDeff = false;
-    /// <summary>
-    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /// </summary>
+    private static readonly Random random = new Random();
+    
     public static List<Soldier> playerArmy = new List<Soldier>();
     public static List<Soldier> enemyArmy = new List<Soldier>();
-    public static Artillery myArtillery = new Artillery(); 
+    
+    public static Artillery myArtillery = new Artillery();
     public static Cavalry myCavalry = new Cavalry();
     public static Infantry myInfantry = new Infantry();
     public static Guard myGuard = new Guard();
     
+    public static Artillery enemyArtillery = new Artillery();
+    public static Cavalry enemyCavalry = new Cavalry();
+    public static Infantry enemyInfantry = new Infantry();
+    public static Guard enemyGuard = new Guard();
+
+    public const int NumberOfInfantry  = 40;
+    public const int NumberOfGuard     = 20;
+    public const int NumberOfCavalry   = 25;
+    public const int NumberOfArtillery = 15;
+    public const int MedicalSupport = 15;
+    public const int DefenseDebaff = 15;
+    public const int UltimateAttack = 10;
+    public const int perUnit = 5;
+    
+    public static int EndOfGame = 0;
+    public static int EnemyLosses = 0;
+    public static int PlayerLosses = 0;
+    public static bool DefenderIsEnemy = false;
+    public static bool PlayerIsSurrender = false;
+    public static bool EnemyIsSurrender = false;
+    public static bool AnyoneIsSurrender = false;
+    public static bool PlayerHasActed = false;
+    public static bool GeneralIsSelectedByEnemy = random.Next(2) == 1;
+    public static bool GeneralIsAttack = false;
+    public static bool GeneralIsDeff = false;
+    public static string PlayerGeneralNameInput { get; set; }
+    public static string EnemyGeneralNameInput = "Жмыхайко Альберт Фон Густаф-Цербстский-Минамото-Ходзё-Наполеон XIX-й";
 }

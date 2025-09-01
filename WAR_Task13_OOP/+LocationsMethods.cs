8e641b2d0d8602ec8bff894d7fbf0a3f674f37f9
Oleg <finkton23@gmail.com>
2025-09-01 14:46:40 +0500

    Update +LocationsMethods.cs

--- WAR_Task13_OOP/+LocationsMethods.cs
+++ WAR_Task13_OOP/+LocationsMethods.cs
@@ -1,56 +1,72 @@
 namespace WAR_Task13_OOP;
 
-public static class LocationsMethods
+public class LocationsMethods
 {
-    public static void HighGrounds()
+    private readonly Data _data;
+    private readonly GameMethods _gameMethods;
+
+    public LocationsMethods(Data data, GameMethods gameMethods)
     {
-        GameMethods.SafeModifyAttack(Data.myArtillery, 0, -20); // MaxAttack -=20
-        Data.myCavalry.Defense -= 25;
-        GameMethods.SafeModifyAttack(Data.myCavalry, -5, -10);
+        _gameMethods = gameMethods;
+        _data = data;
+    }
+    public void HighGrounds()
+    {
+        _gameMethods.SafeModifyAttack(_data.myArtillery, 0, -20); // MaxAttack -=20
+        _data.myCavalry.Defense -= 25;
+        _gameMethods.SafeModifyAttack(_data.myCavalry, -5, -10);
     
-        Data.myInfantry.Defense += 25;
-        GameMethods.SafeModifyAttack(Data.myInfantry, 5, 10);
+        _data.myInfantry.Defense += 25;
+        _gameMethods.SafeModifyAttack(_data.myInfantry, 5, 10);
     
-        Data.myGuard.Defense += 25;
-        GameMethods.SafeModifyAttack(Data.myGuard, 5, 10);
+        _data.myGuard.Defense += 25;
+        _gameMethods.SafeModifyAttack(_data.myGuard, 5, 10);
     }
 
-    public static void City()
+    public void City()
     {
-        GameMethods.SafeModifyAttack(Data.myArtillery, 0, -15);
-        GameMethods.SafeModifyAttack(Data.myCavalry, 10, 15);
+        _gameMethods.SafeModifyAttack(_data.myArtillery, 0, -15);
+        _gameMethods.SafeModifyAttack(_data.myCavalry, 10, 15);
     
-        Data.myInfantry.Defense += 25;
-        GameMethods.SafeModifyAttack(Data.myInfantry, -10, -15);
+        _data.myInfantry.Defense += 25;
+        _gameMethods.SafeModifyAttack(_data.myInfantry, -10, -15);
     
-        Data.myGuard.Defense += 25;
-        GameMethods.SafeModifyAttack(Data.myGuard, -10, -15);
+        _data.myGuard.Defense += 25;
+        _gameMethods.SafeModifyAttack(_data.myGuard, -10, -15);
     }
 
-    public static void Plane()
+    public void Plane()
     {
-        GameMethods.SafeModifyAttack(Data.myArtillery, 10, 10);
-        Data.myGuard.Defense -= 15;
-        Data.myInfantry.Defense -= 15;
-        Data.myCavalry.Defense += 15;
-        GameMethods.SafeModifyAttack(Data.myCavalry, 10, 15);
+        if (_data == null)
+        {
+            throw new Exception("Ошибка: _data в LocationsMethods == null");
+        }
+        if (_data.myGuard == null)
+        {
+            throw new Exception("Ошибка: _data.myGuard == null");
+        }
+        _gameMethods.SafeModifyAttack(_data.myArtillery, 10, 10);
+        _data.myGuard.Defense -= 15;
+        _data.myInfantry.Defense -= 15;
+        _data.myCavalry.Defense += 15;
+        _gameMethods.SafeModifyAttack(_data.myCavalry, 10, 15);
     }
 
-    public static void Forest()
+    public void Forest()
     {
         // Артиллерия - только уменьшение MaxAttack
-        GameMethods.SafeModifyAttack(Data.myArtillery, 0, -10); // MaxAttack -=10, MinAttack без изменений
+        _gameMethods.SafeModifyAttack(_data.myArtillery, 0, -10); // MaxAttack -=10, MinAttack без изменений
     
         // Кавалерия - уменьшение всех характеристик
-        Data.myCavalry.Defense -= 20;
-        GameMethods.SafeModifyAttack(Data.myCavalry, -10, -20); // MinAttack -=10, MaxAttack -=20
+        _data.myCavalry.Defense -= 20;
+        _gameMethods.SafeModifyAttack(_data.myCavalry, -10, -20); // MinAttack -=10, MaxAttack -=20
     
         // Пехота - усиление
-        Data.myInfantry.Defense += 20;
-        GameMethods.SafeModifyAttack(Data.myInfantry, 10, 15); // MinAttack +=10, MaxAttack +=15
+        _data.myInfantry.Defense += 20;
+        _gameMethods.SafeModifyAttack(_data.myInfantry, 10, 15); // MinAttack +=10, MaxAttack +=15
     
         // Гвардия - усиление
-        Data.myGuard.Defense += 20;
-        GameMethods.SafeModifyAttack(Data.myGuard, 10, 15); // MinAttack +=10, MaxAttack +=15
+        _data.myGuard.Defense += 20;
+        _gameMethods.SafeModifyAttack(_data.myGuard, 10, 15); // MinAttack +=10, MaxAttack +=15
     }
 }

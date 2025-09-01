56ab551236fd04a4450138da3c30eb23acfeea43
Oleg <finkton23@gmail.com>
2025-09-01 14:45:10 +0500

    Update +GameCycle.cs

--- WAR_Task13_OOP/+GameCycle.cs
+++ WAR_Task13_OOP/+GameCycle.cs
@@ -1,8 +1,25 @@
-﻿namespace WAR_Task13_OOP;
+namespace WAR_Task13_OOP;
 
-public static class GameCycle
+public class GameCycle
 {
-    public static void GameMenu()
+    private Data _data;
+    private GameMethods _gameMethods;
+    private LocationsMethods _locationsMethods;
+    private BattleInterface _battleInterface;
+    private AttackGeneral _attackGeneral;
+    private DeffGeneral _deffGeneral;
+
+    public GameCycle()
+    {
+        _data = new Data();
+        _gameMethods = new GameMethods(_data);
+        _locationsMethods = new LocationsMethods(_data, _gameMethods);
+        _attackGeneral = new AttackGeneral(_data.PlayerGeneralNameInput, Data.GeneralAttackBuff, _data);
+        _deffGeneral = new DeffGeneral(_data.PlayerGeneralNameInput, Data.GeneralDefenseBuff, _data);
+        _battleInterface = new BattleInterface();
+    }
+
+    public void GameMenu()
     {
         Console.WriteLine("Чтобы начать, нажмите ENTER...");
         string input = Console.ReadLine();
@@ -32,7 +49,7 @@ public static class GameCycle
         }
     }
 
-    private static void GameSettings()
+    private void GameSettings()
     {
         Console.Clear();
         Console.WriteLine("\nВыберите командующего:");
@@ -42,24 +59,24 @@ public static class GameCycle
         switch (keyInfo.Key)
         {
             case ConsoleKey.D1:
-                Data.GeneralIsAttack = true;
-                AttackGeneral.AttackBuff();
+                _data.GeneralIsAttack = true;
+                _attackGeneral.AttackBuff();
                 break;
             case ConsoleKey.D2:
-                Data.GeneralIsDeff = true;
-                DeffGeneral.DeffenseBuff();
+                _data.GeneralIsDeff = true;
+                _deffGeneral.DeffenseBuff();
                 break;
             default:
                 Console.WriteLine("Неправильный выбор!");
                 break;
         }
-        
-        if (Data.GeneralIsAttack == true || Data.GeneralIsDeff == true)
+
+        if (_data.GeneralIsAttack == true || _data.GeneralIsDeff == true)
         {
             Console.WriteLine("\nНазовите командующего:");
             string input = Console.ReadLine()?.Trim();
-            Data.PlayerGeneralNameInput = string.Join(" ", input);
-            if (Data.PlayerGeneralNameInput != null || Data.PlayerGeneralNameInput != null)
+            _data.PlayerGeneralNameInput = string.Join(" ", input);
+            if (_data.PlayerGeneralNameInput != null || _data.PlayerGeneralNameInput != null)
             {
                 Console.Clear();
                 Console.WriteLine("\nВыберите локацию:");
@@ -71,34 +88,34 @@ public static class GameCycle
                 switch (keyInfo2.Key)
                 {
                     case ConsoleKey.D1:
-                        LocationsMethods.Plane();
-                        GameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _locationsMethods.Plane();
+                        _gameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
-                        GameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _gameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
                         GameBattle();
                         break;
                     case ConsoleKey.D2:
-                        LocationsMethods.Forest();
-                        GameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _locationsMethods.Forest();
+                        _gameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
-                        GameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _gameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
                         GameBattle();
                         break;
                     case ConsoleKey.D3:
-                        LocationsMethods.HighGrounds();
-                        GameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _locationsMethods.HighGrounds();
+                        _gameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
-                        GameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _gameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
                         GameBattle();
                         break;
                     case ConsoleKey.D4:
-                        LocationsMethods.City();
-                        GameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _locationsMethods.City();
+                        _gameMethods.FillPlayerArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
-                        GameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
+                        _gameMethods.FillEnemyArmyFromTemplates(Data.NumberOfInfantry, Data.NumberOfGuard,
                             Data.NumberOfCavalry, Data.NumberOfArtillery);
                         GameBattle();
                         break;
@@ -110,16 +127,16 @@ public static class GameCycle
         }
     }
 
-    public static void GameBattle()
+    public void GameBattle()
     {
-        Console.Clear();
-        while (Data.playerArmy.Count > 0 && Data.enemyArmy.Count > 0)
+        //Console.Clear();
+        while (_data.playerArmy.Count > 0 && _data.enemyArmy.Count > 0)
         {
-            while (!Data.PlayerIsSurrender && Data.playerArmy.Count != 0 && Data.enemyArmy.Count != 0)
+            while (!_data.PlayerIsSurrender && _data.playerArmy.Count != 0
+                                            && _data.enemyArmy.Count != 0)
             {
-
                 Console.WriteLine("\nБоевое поле:");
-                BattleInterface.DrawBattleVisor(Data.playerArmy, Data.enemyArmy);
+                _battleInterface.DrawBattleVisor(_data.playerArmy, _data.enemyArmy);
                 ///////////////////////////////////////////////////////////////////////////////////////////////////////
                 Console.WriteLine("\nВыберите действие, командир:");
                 Console.WriteLine("\n+ 1. Атака.");
@@ -132,89 +149,96 @@ public static class GameCycle
                 switch (keyInfo2.Key)
                 {
                     case ConsoleKey.D1:
-                        Data.DefenderIsEnemy = true;
-                        GameMethods.Attacking(Data.playerArmy, Data.enemyArmy);
-                        int killed = GameMethods.Attacking(Data.playerArmy, Data.enemyArmy);
+                        _data.DefenderIsEnemy = true;
+                        int killed = _gameMethods.Attacking(_data.playerArmy,
+                            _data.enemyArmy);
                         Console.WriteLine($"Ваша армия наступает на позиции противника! " +
                                           $"Потери противника: {killed} чел.");
-                        Data.PlayerHasActed = true;
+                        _data.PlayerHasActed = true;
                         break;
                     case ConsoleKey.D2:
-                        Data.DefenderIsEnemy = true;
-                        GameMethods.SuperAttacking(Data.playerArmy, Data.enemyArmy);
-                        int killed2 = GameMethods.SuperAttacking(Data.playerArmy, Data.enemyArmy);
+                        _data.DefenderIsEnemy = true;
+                        int killed2 = _gameMethods.SuperAttacking(_data.playerArmy,
+                            _data.enemyArmy);
                         Console.WriteLine($"Ваша армия проводит отчаянную атаку на позиции противника! " +
                                           $"Потери противника: {killed2} чел.");
-                        Data.PlayerHasActed = true;
+                        _data.PlayerHasActed = true;
                         break;
                     case ConsoleKey.D3:
-                        Data.DefenderIsEnemy = true;
-                        GameMethods.Defending(Data.playerArmy);
-                        int perUnit = GameMethods.Defending(Data.playerArmy);
+                        _data.DefenderIsEnemy = true;
+                        int perUnit = _gameMethods.Defending(_data.playerArmy);
                         Console.WriteLine($"Ваша армия проводит укрепление траншей! " +
                                           $"Укрепленность увеличилась на: {perUnit}, на бойца, единиц на ход.");
-                        Data.PlayerHasActed = true;
+                        _data.PlayerHasActed = true;
                         break;
                     case ConsoleKey.D4:
-                        Data.DefenderIsEnemy = true;
-                        GameMethods.MedicalUnitRemoval(Data.playerArmy);
-                        int healed = GameMethods.MedicalUnitRemoval(Data.playerArmy);
+                        _data.DefenderIsEnemy = true;
+
+                        int healed = _gameMethods.MedicalUnitRemoval(_data.playerArmy);
                         Console.WriteLine($"МедСанБат прибыл, и вылечил раненных солдат! " +
                                           $"Вылечено бойцов: {healed}.");
-                        Data.PlayerHasActed = true;
+                        _data.PlayerHasActed = true;
                         break;
                     case ConsoleKey.D5:
-                        Data.DefenderIsEnemy = true;
-                        GameMethods.DefendingDebaff(Data.enemyArmy);
+                        _data.DefenderIsEnemy = true;
+                        _gameMethods.DefendingDebaff(_data.enemyArmy);
                         Console.WriteLine($"Ваша разведка провела диверсию в логистических цепочках противника! " +
                                           $"Обороноспособность противника упала на: {Data.DefenseDebaff} единиц.");
                         break;
                     case ConsoleKey.D6:
-                        Data.DefenderIsEnemy = true;
-                        if (Data.playerArmy.Count < 60)
+                        _data.DefenderIsEnemy = true;
+                        if (_data.playerArmy.Count < 60)
                         {
                             Console.WriteLine("Ваша армия понесла большие потери! Вы командуете ей отступить" +
                                               "и сдать позиции врагу!");
-                            Data.PlayerIsSurrender = true;
+                            _data.PlayerIsSurrender = true;
+                            GameMenu();
                         }
-                        else if (Data.playerArmy.Count > 60)
+                        else if (_data.playerArmy.Count > 60)
                         {
                             Console.WriteLine("Вы не можете отступить! У вас ещё есть все ресурсы чтобы переломить " +
                                               "ход боя в свою пользу! Высшее командование не одобрит сдачу позиций!");
                             return;
                         }
-                            break;
+
+                        break;
                     default:
                         Console.WriteLine("Неправильный выбор!");
                         return;
                         break;
                 }
-                Data.playerArmy.RemoveAll(s => !s.IsAlive);
-                Data.enemyArmy.RemoveAll(s => !s.IsAlive);
+
                 Console.WriteLine("Чтобы продолжить нажмите ENTER.");
                 string input = Console.ReadLine();
-                if (Data.PlayerIsSurrender == true || Data.EnemyIsSurrender == true)
+                if (_data.PlayerIsSurrender == true || _data.EnemyIsSurrender == true)
                 {
-                    GameMethods.Surrender();
+                    _gameMethods.Surrender();
                 }
-                if (Data.playerArmy.Count <= 0)
+
+                if (_data.playerArmy.Count <= 0)
                 {
-                    Console.WriteLine($"Генерал {Data.EnemyGeneralNameInput} (враг) со своим войском молниеносно разгромил " +
-                                      $"войско вражеского генерала {Data.PlayerGeneralNameInput}!!!");
+                    Console.WriteLine(
+                        $"Генерал {_data.EnemyGeneralNameInput} (враг) со своим войском молниеносно разгромил " +
+                        $"войско вражеского генерала {_data.PlayerGeneralNameInput}!!!");
                     GameMenu();
                 }
-                else if (Data.enemyArmy.Count <= 0)
+                else if (_data.enemyArmy.Count <= 0)
                 {
-                    Console.WriteLine($"Генерал {Data.PlayerGeneralNameInput} (вы) со своим войском молниеносно разгромил " +
-                                      $"войско вражеского генерала {Data.EnemyGeneralNameInput}!!!");
+                    Console.WriteLine(
+                        $"Генерал {_data.PlayerGeneralNameInput} (вы) со своим войском молниеносно разгромил " +
+                        $"войско вражеского генерала {_data.EnemyGeneralNameInput}!!!");
                     GameMenu();
                 }
-                if (Data.PlayerHasActed == true && input == "")
+
+                if (_data.PlayerHasActed == true && input == "")
                 {
                     Console.WriteLine("Ход противника.");
-                    Data.DefenderIsEnemy = false;
-                    GameMethods.EnemyTurn();
+                    _data.DefenderIsEnemy = false;
+                    _gameMethods.EnemyTurn();
                 }
+
+                _data.playerArmy.RemoveAll(s => !s.IsAlive);
+                _data.enemyArmy.RemoveAll(s => !s.IsAlive);
             }
         }
     }
